c1462b053bc6779c61f46d5c6b1e718c66ae523f
agus <smartgdi@gmail.com>
2013-08-03 14:13:32 +0700

    Edit readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ realtime-notif-socket.io
 ========================
 
 Demo of realtime notification with socket.io and nodejs
+Requirements:
+- nodejs
+- socket.io
+
+Run:
+node server_v1.js
+then open file index_v1.html
--- server_v1.js
+++ server_v1.js
@@ -6,30 +6,9 @@ app.listen(3000);
 function handler(req, res) {}
 
 io.sockets.on('connection', function(socket) {
-	socket.on('message', function(msg) {
-		//socket.emit('message', {message: msg});
-		// Broadcast pesan ke semua client yg terhubung
-		socket.broadcast.emit('message', msg);
-	});
-
-	// saat ada berita baru ditambah
-	socket.on('article_added', function(msg) {
-		// Broadcast pesan ke semua client yg terhubung
-		socket.broadcast.emit('article_added', msg);
-	});
-
-
-	// saat ada lowongan baru ditambah
-	socket.on('vacancy_added', function(msg) {
-		//socket.emit('message', {message: msg});
-		// Broadcast pesan ke semua client yg terhubung
-		socket.broadcast.emit('vacancy_added', msg);
-	});
-
-	// saat ada yg apply lowongan  	
-	socket.on('vacancy_applied', function(msg) {
-		//socket.emit('message', {message: msg});
-		// Broadcast pesan ke semua client yg terhubung
-		socket.broadcast.emit('vacancy_applied', msg);
-	});  
+  socket.on('message', function(msg) {
+    //socket.emit('message', {message: msg});
+    socket.broadcast.emit('message', msg);
+    // console.log(msg);
+  });
 });
